ba7b97309869f3c97646a4e859c6afbd02708ad1
Rob Sherman <RadianWeb@192-168-1-112.tpgi.com.au>
2019-04-07 18:06:22 +1000

    Set Up Gulp Watch

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+
+gulp.task('sass', function(){
+  return gulp.src('scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+});
+
+// Minifys .css and reload browser.
+gulp.task('mini-css', function() {
+    return gulp.src('styles.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('./'));
+});
+
+// Compile and minify css.
+gulp.task('do-sass', gulp.series('sass', 'mini-css'));
+
+
+gulp.task('watch', function(){
+  gulp.watch('scss/**/*.scss', gulp.series('do-sass')); 
+  // Other watchers
+});
